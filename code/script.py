6cc4f5d06d96da9704fc4f7a5f2724fc76ef9695
Lele Chen <lchen63@ur.rochester.edu>
2020-08-29 15:02:05 -0400

    Update script.py

--- code/script.py
+++ code/script.py
@@ -15,3 +15,5 @@ def main():
             command = 'python demo.py -i ' + audio_path + ' -p ' + image_path + '  -s ' + save_name
             print  (command)
             #python demo.py  -i ../dataset/test_data/test_audio/angry_yousefu_00034.wav -p ../dataset/test_data/test_image/yousef/1.jpg -s angry_yousefu_00034__1
+
+main()
