cd87e1b141eca3272550c5d638744ce6d25784d2
Lele Chen <lchen63@ur.rochester.edu>
2020-08-29 15:01:18 -0400

    Update script.py

--- code/script.py
+++ code/script.py
@@ -1,1 +1,17 @@
- python demo.py  -i ../dataset/test_data/test_audio/angry_yousefu_00034.wav -p ../dataset/test_data/test_image/yousef/1.jpg -s angry_yousefu_00034__1
+import os
+
+def main():
+    audio_list = os.listdir('/u/lchen63/ATVGnet_cvpr2020/dataset/test_data/test_audio')  
+    
+    image_list = audio_list = os.listdir('/u/lchen63/ATVGnet_cvpr2020/dataset/test_data/test_image')
+    
+    for imge in image_list:
+        image_path = os.path.join('/u/lchen63/ATVGnet_cvpr2020/dataset/test_data/test_image', imge, '1.jpg')
+        
+        for audio in audio_list:
+            audio_path = os.path.join('/u/lchen63/ATVGnet_cvpr2020/dataset/test_data/test_audio', audio)
+            
+            save_name = audio[:-4] + '__' +  imge + '__1.jpg'
+            command = 'python demo.py -i ' + audio_path + ' -p ' + image_path + '  -s ' + save_name
+            print  (command)
+            #python demo.py  -i ../dataset/test_data/test_audio/angry_yousefu_00034.wav -p ../dataset/test_data/test_image/yousef/1.jpg -s angry_yousefu_00034__1
