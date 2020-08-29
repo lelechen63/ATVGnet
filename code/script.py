1dd622c62c0d7fa1608c040f4464684dfa492392
Lele Chen <lchen63@ur.rochester.edu>
2020-08-29 15:04:31 -0400

    Update script.py

--- code/script.py
+++ code/script.py
@@ -9,6 +9,7 @@ def main():
         image_path = os.path.join('/u/lchen63/ATVGnet_cvpr2020/dataset/test_data/test_image', imge, '1.jpg')
         
         for audio in audio_list:
+            print (audio)
             audio_path = os.path.join('/u/lchen63/ATVGnet_cvpr2020/dataset/test_data/test_audio', audio)
             
             save_name = audio[:-4] + '__' +  imge + '__1.jpg'
